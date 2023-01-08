88e8d247dd28bcd98b14ee5c7c9f0ea3994ed079
Todd B. Smith <elliptic1@gmail.com>
2023-01-08 13:36:50 -0500

    Initial commit

---
+++ app/src/main/java/com/tbse/basicstatecodelab/ui/composables/WellnessScreen.kt
@@ -0,0 +1,14 @@
+package com.tbse.basicstatecodelab.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Created by toddsmith on 1/8/23.
+ */
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+) {
+    WaterCounter(modifier)
+}
